0d98f2f2ed8fe9e82ddcac4bde8365b26c43869d
Florian Wartner <florian@wartner.io>
2022-09-07 10:21:35 +0200

    Fixes

--- src/app.js
+++ src/app.js
@@ -20,28 +20,15 @@ client.subscribe(process.env.MQTT_TOPIC)
 
 
 client.on('message', (topic, message) => {
-    // console.log(topic, JSON.parse(message))
+    console.log(topic, JSON.parse(message))
     getStream(JSON.parse(message))
 })
 
 function getStream(data) {
-    var sendAndSleep = function(response, counter) {
-        if (counter > 10) {
-            response.end();
-        } else {
-            response.write(" ;i=" + counter);
-            counter++;
-            setTimeout(function() {
-                sendAndSleep(response, counter);
-            }, 1000)
-        };
-    };
-
     app.get('/stream', function(req, res, next) {
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.write(JSON.stringify(data));
         res.end();
-        // sendAndSleep(res, 1);
     });
 
     app.listen(port, () => {
--- src/client.js
+++ src/client.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 
 var mqtt_host = process.env.MQTT_HOST;
 var mqtt_port = process.env.MQTT_PORT;
-var mqtt_topic = process.env.MQTT_TOPIC;
 
 var options = {
     clientId: process.env.MQTT_CLIENT_ID,
